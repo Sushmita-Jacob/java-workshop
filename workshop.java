8c735b00e19ab5cc31c8929471b0441d6f8ce3c7
Sushmita-Jacob <sushmita.cjacob@gmail.com>
2026-07-18 11:57:28 -0400

    Workshop

---
+++ workshop.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        int x = 5;
+        double y = 5.5;
+        boolean k = false;
+        char a = 'a';
+        String 100 = "Hello world!";
+        String greeting = "Hello!";
+        String greetingname = "Hello\nSushmita";
+        String sameline = "This will say \\n, not a new line.";
+        System.out.println(greeting);
+        System.out.println(greetingname);
+    }
+}
