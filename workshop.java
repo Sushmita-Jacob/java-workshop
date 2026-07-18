ae28c917602746958c99f74c5a39b53cc56ff4f4
Sushmita-Jacob <sushmita.cjacob@gmail.com>
2026-07-18 12:47:14 -0400

    Workshop 2

--- workshop.java
+++ workshop.java
@@ -1,14 +1,27 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        int x = 5;
-        double y = 5.5;
+        // String is non-primitive, so use .equals()
+        // AND OR NOT
+        int intx = 5;
+        int inty = 10;
+        double doublex = 5;
+        double doubley = 10;
+        double x = Math.pow(3,2) // 3^2 =9
+        // Double uses more memory, int for optimization (stick to doubles)
+        // Int truncates
         boolean k = false;
         char a = 'a';
+        // Increments: x++ as x+= or x-- as x-=
+        // Adding a string and an integer causes concatination, not addition
         String 100 = "Hello world!";
         String greeting = "Hello!";
         String greetingname = "Hello\nSushmita";
-        String sameline = "This will say \\n, not a new line.";
-        System.out.println(greeting);
-        System.out.println(greetingname);
+        if (intx == 5) {
+            String sameline = "This will say \\n, not a new line.";
+        }
+        System.out.println(Math.sqrt(intx + inty));
+        if (true) {
+            System.out.println(greetingname);
+        }
     }
 }
